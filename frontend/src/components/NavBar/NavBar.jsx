4972c1c1fbd89de82f15191aef8c43906bf4fd72
Adam Davidson <agdavidson91@gmail.com>
2022-05-17 13:34:36 -0500

    added styles to navbar. Added about me styling

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,8 +3,6 @@ import "./NavBar.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 import AuthContext from "../../context/AuthContext";
-import HomePage from "../../pages/HomePage/HomePage";
-import ProfilePage from "../../pages/ProfilePage/ProfilePage";
 import React from "react";
 import { useContext } from "react";
 
@@ -91,76 +89,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-{
-  /* <ul style={{ border: "1px solid black", flexGrow: "1" }}>
-        <li>
-          <Link
-            className="brand"
-            to="/"
-            style={{ textDecoration: "none", color: "black" }}
-          >
-            <b>Froodie Book</b>
-          </Link>
-        </li>
-      </ul>
-      <ul
-        style={{
-          border: "1px solid black",
-          flexGrow: "2",
-          //   marginBottom: "1em",
-          flexDirection: "column",
-        }}
-      >
-        <li style={{ border: "1px solid black" }}>
-          <Link to="/">
-            <p>Home / Feed</p>
-          </Link>
-        </li>
-        <li style={{ border: "1px solid black" }}>
-          <Link to="/myrecipepage">
-            <p>My Recipes Page</p>
-          </Link>
-        </li>
-        <li style={{ border: "1px solid black" }}>
-          <Link to="/favoritespage">
-            <p>Favorites Page</p>
-          </Link>
-        </li>
-        <li style={{ border: "1px solid black" }}>
-          <Link to="/profilepage">
-            <p>Profile Page</p>
-          </Link>
-        </li>
-      </ul>
-      <div>
-        <ul
-          style={{
-            border: "3px solid red",
-            flexGrow: "2",
-            flexDirection: "column",
-          }}
-        >
-          <li style={{ border: "1px solid black" }}>
-            <Link to="/">
-              <p>Home / Feed</p>
-            </Link>
-          </li>
-          <li style={{ border: "1px solid black" }}>
-            <Link to="/myrecipepage">
-              <p>My Recipes Page</p>
-            </Link>
-          </li>
-          <li style={{ border: "1px solid black" }}>
-            <Link to="/favoritespage">
-              <p>Favorites Page</p>
-            </Link>
-          </li>
-          <li style={{ border: "1px solid black" }}>
-            <Link to="/profilepage">
-              <p>Profile Page</p>
-            </Link>
-          </li>
-        </ul>
-        </div>*/
-}
