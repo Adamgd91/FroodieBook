be9d321fc26f19a205f7e7de74d3c73b1ec79647
Adam Davidson <agdavidson91@gmail.com>
2022-05-17 12:23:30 -0500

    working on recipes

--- frontend/src/pages/MyRecipesPage/MyRecipesPage.jsx
+++ frontend/src/pages/MyRecipesPage/MyRecipesPage.jsx
@@ -1,7 +1,9 @@
+import "./Recipes.scss";
+
 import React from "react";
 
 const MyRecipesPage = () => {
-  return <div>MyRecipes</div>;
+  return <div className="recipe-container">MyRecipes</div>;
 };
 
 export default MyRecipesPage;
