e8c3fe0475651c4ac8a49d8ce99601f89ce2ce70
Adam Davidson <agdavidson91@gmail.com>
2022-05-23 20:17:54 -0500

    added image frontend. still need to complete but in a good spot

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import "./NavBar.scss";
 import { Link, useNavigate } from "react-router-dom";
 
 import AuthContext from "../../context/AuthContext";
+import ImageUpload from "../ImageUpload/ImageUpload.";
 import React from "react";
 import { useContext } from "react";
 
@@ -11,81 +12,86 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <div className="navs">
-    <div className="navBar">
-      <div className="header-container test">
-        <ul className="logo-navbar">
-          <li>
-            <Link to="/" className="nav-links">
-              Froodie Book
-            </Link>
-          </li>
-        </ul>
-        <section>Add Image</section>
-        <section>{user.name}</section>
-      </div>
-      <div className="user-container test">
-        <ul className="navbar-nav-links">
-          <li>
-            <Link to="/" className="nav-links">
-              <p>Home / Feed</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/myrecipespage" className="nav-links">
-              <p>My Recipes</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/favoritespage" className="nav-links">
-              <p>Favorites</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/profilepage" className="nav-links">
-              <p>Profile</p>
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="friends-container test">
-        {" "}
-        <ul className="navbar-nav-links">
-          <li>
-            <Link to="/friendspage" className="nav-links">
-              <p>Friends</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/friendspage" className="nav-links">
-              <p>Pending Friends</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/friendspage" className="nav-links">
-              <p>Friends Request</p>
-            </Link>
-          </li>
-          <li>
-            <Link to="/foodgeneratorpage" className="nav-links">
-              <p>Food Generator</p>
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <div className="navBar">
+        <div className="header-container test">
+          <ul className="logo-navbar">
+            <li>
+              <Link to="/" className="nav-links">
+                Froodie Book
+              </Link>
+            </li>
+          </ul>
+          <section>
+            <ImageUpload />
+          </section>
+          <section>{user.name}</section>
+        </div>
+        <div className="user-container test">
+          <ul className="navbar-nav-links">
+            <li>
+              <Link to="/" className="nav-links">
+                <p>Home / Feed</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/myrecipespage" className="nav-links">
+                <p>My Recipes</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/favoritespage" className="nav-links">
+                <p>Favorites</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/profilepage" className="nav-links">
+                <p>Profile</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="friends-container test">
+          {" "}
+          <ul className="navbar-nav-links">
+            <li>
+              <Link to="/friendspage" className="nav-links">
+                <p>Friends</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/friendspage" className="nav-links">
+                <p>Pending Friends</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/friendspage" className="nav-links">
+                <p>Friends Request</p>
+              </Link>
+            </li>
+            <li>
+              <Link to="/foodgeneratorpage" className="nav-links">
+                <p>Food Generator</p>
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-      <div className="logout-container test">
-        {" "}
-        {user ? (
-          <button className="logoutbuttons" onClick={() => navigate("/login")}>
-            Logout
-          </button>
-        ) : (
-          <button className="logoutbutton" onClick={() => navigate("/login")}>
-            Login
-          </button>
-        )}
+        <div className="logout-container test">
+          {" "}
+          {user ? (
+            <button
+              className="logoutbuttons"
+              onClick={() => navigate("/login")}
+            >
+              Logout
+            </button>
+          ) : (
+            <button className="logoutbutton" onClick={() => navigate("/login")}>
+              Login
+            </button>
+          )}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
