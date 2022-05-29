d7dfafce0ae7ee9a7488946651b101b5d11b0d41
Adam Davidson <agdavidson91@gmail.com>
2022-05-29 09:29:59 -0500

    fixed login route when starting app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,11 @@ import AddRecipePage from "./pages/AddRecipePage/AddRecipePage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import FavoritesPage from "./pages/FavoritesPage/FavoritesPage";
 import FoodGeneratorPage from "./pages/FoodGeneratorPage/FoodGeneratorPage";
-import Footer from "./components/Footer/Footer";
 import FriendsPage from "./pages/FriendsPage/FriendsPage";
 import HomePage from "./pages/HomePage/HomePage";
 import Layout from "./Layout";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import MyRecipePage from "./pages/MyRecipesPage/MyRecipesPage";
-import Navbar from "./components/NavBar/NavBar";
 import PrivateRoute from "./utils/PrivateRoute";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -24,7 +22,14 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
-        <Route path="/" element={<Layout />}>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Layout />
+            </PrivateRoute>
+          }
+        >
           <Route path="/" element={<HomePage />} />
           <Route path="/profilepage" element={<ProfilePage />} />
           <Route path="/myrecipespage" element={<MyRecipePage />} />
